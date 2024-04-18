22ecf53cba98ad556a65500be10dd65c022d0d97
Diontae Akins <diontaea@gmail.com>
2024-04-18 10:55:31 -0400

    2

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
 // Menu data structure
 var menuLinks = [
-    { text: 'about', href: '/about' },
-    { text: 'catalog', href: '/catalog' },
-    { text: 'orders', href: '/orders' },
-    { text: 'account', href: '/account' },
-  ];
+  {text: 'about', href: '/about'},
+  {text: 'catalog', href: '#', subLinks: [
+    {text: 'all', href: '/catalog/all'},
+    {text: 'top selling', href: '/catalog/top'},
+    {text: 'search', href: '/catalog/search'},
+  ]},
+  {text: 'orders', href: '#' , subLinks: [
+    {text: 'new', href: '/orders/new'},
+    {text: 'pending', href: '/orders/pending'},
+    {text: 'history', href: '/orders/history'},
+  ]},
+  {text: 'account', href: '#', subLinks: [
+    {text: 'profile', href: '/account/profile'},
+    {text: 'sign out', href: '/account/signout'},
+  ]},
+];
 
 
 
 const mainEl = document.querySelector("main");
+const subMenuEl= document.getElementById("sub-menu");
 const topMenuEl = document.getElementById("top-menu");
 
 
 mainEl.style.backgroundColor = 'var(--main-bg)';
 topMenuEl.style.height = '100%';
 topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
+
+subMenuEl.style.height = '100%';
+subMenuEl.style.backgroundColor = 'var(--sub-menu-bg)';
+
+subMenuEl.classList.add('flex-around');
+subMenuEl.style.position = 'absolute';
+subMenuEl.style.top = '0';
+
+
 
 
 const h1 = document.createElement('h1');
@@ -40,11 +61,3 @@ topMenuEl.appendChild(menuLink)
 
 });
 
-//   for (let i = 0; i < menuLinks.length; i++) {
-//    
-//     anchor.textContent = menuLinks[i]
-//      
-
- 
-// }
-//  anchor.getAttribute('menuLinks'); 
